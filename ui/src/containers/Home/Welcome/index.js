323e8806ebbc61bc898c33663544c7ac31908dbf
ducthaidev@gmail.com <Thai.1201>
2021-03-17 12:09:15 +0700

    change welcome

--- ui/src/containers/Home/Welcome/index.js
+++ ui/src/containers/Home/Welcome/index.js
@@ -7,10 +7,12 @@ const Welcome = () => {
   const categories = useSelector((state) => state.category.categories);
   return (
     <div className="welcome">
-      <Section
-        categories={categories || categories}
-        action={{ type: 'newest', limit: 6 }}
-      />
+      {categories ? (
+        <Section
+          categories={categories}
+          action={{ type: 'newest', limit: 6 }}
+        />
+      ) : null}
     </div>
   );
 };
